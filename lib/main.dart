45c48c6da4f1deed3bfd45592cbde9472d19dbdf
Nikolai Ilushko <kolyaxil@gmail.com>
2018-07-28 12:56:18 +0300

    Learning Dart!

---
+++ lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/widgets.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyFirstWidget());
+}
+
+class MyFirstWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    Text myText = Text("This is a nice photo", textDirection: TextDirection.ltr);
+
+    Image myImage = Image.network("https://images.pexels.com/photos/1168940/pexels-photo-1168940.jpeg");
+
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: Text("MyApp")),
+        body: Center(
+          child: Center(
+            children: <Widget>[myText, myImage],
+          )
+        )
+      )
+    )
+
+    return Center(child: Column(
+      children: <Widget>[myText, myImage],
+      mainAxisSize: MainAxisSize.min
+    ));
+  }
+}
+
+
+
