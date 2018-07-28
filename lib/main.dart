a2fa867de8b688bb000546575a6df6ca980e5b7b
Nikolai Ilushko <kolyaxil@gmail.com>
2018-07-28 13:33:45 +0300

    Final stages, wants to find a way to make the thing work.

--- lib/main.dart
+++ lib/main.dart
@@ -2,12 +2,13 @@ import 'package:flutter/widgets.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyFirstWidget());
+  runApp(MySecondWidget());
 }
 
 class MyFirstWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+
     Text myText = Text("This is a nice photo", textDirection: TextDirection.ltr);
 
     Image myImage = Image.network("https://images.pexels.com/photos/1168940/pexels-photo-1168940.jpeg");
@@ -24,6 +25,53 @@ class MyFirstWidget extends StatelessWidget {
       )
     );
   }
+}
+
+class MySecondWidget extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return MyState();
+  }
+}
+
+class MyState extends State<MySecondWidget> {
+  String url = "https://source.unsplash.com/random/800x600";
+  // A random image from Unsplash
+
+  @override
+  void setState(VoidCallback fn) {
+    url = "https://source.unsplash.com/random/800x600/?" +
+          "q=${new DateTime.now().millisecondsSinceEpoch}";
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: Scaffold(
+            body: Center(
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: <Widget>[
+                    RaisedButton(
+                      child: Text("Press Me"),
+                      onPressed: changeURL,
+                    ),
+                    Image.network(url)
+                  ],
+                )
+            )
+        )
+    );
+  }
+
+  void changeURL() {
+   this.setState(() {
+     url = "https://source.unsplash.com/random/800x600/?" +
+         "q=${new DateTime.now().millisecondsSinceEpoch}";
+   });
+  }
+
+
 }
 
 
